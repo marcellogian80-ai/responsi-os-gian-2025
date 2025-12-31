d404d06207e2aea19320e9e88e0ff5aabca23d40
marcellogian80-ai <marcellogian80@gmail.com>
2025-12-31 13:17:13 +0700

    UPDATE: Disini saya coba menambahkan link Github

--- tugas_responsi.sh
+++ tugas_responsi.sh
@@ -11,4 +11,7 @@ echo "- WSL Distro: Ubuntu 24.04 LTS"
 echo ""
 echo "Docker Status:"
 docker --version 2>/dev/null || echo "Docker tidak terinstall"
+echo "GitHub URL: https://github.com/marcellogian80-ai/responsi-os-gian-2025"
+
+
 
